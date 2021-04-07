0bccc3a9b8781f2bee84172e677772a6a9ac1e6a
HenyHamdiyah <63156702+HenyHamdiyah@users.noreply.github.com>
2021-04-07 07:07:33 +0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,16 @@
+print ("")
+print ("====================== BIODATA ======================")
+print ("")
+
+nim = input("NIM : ")
+nama_lengkap = input("Nama Lengkap : ")
+fakultas = input("Fakultas : ")
+prodi = input("Prodi : ")
+jenis_kelamin = input("Jenis Kelamin : ")
+agama = input("Agama : ")
+gol_darah = input("Gol Darah : ")
+tmp_tgl_lahir = input("Tmp & Tgl Lahir : ")
+alamat = input("Alamat : ")
+
+
+
